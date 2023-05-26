6195d57ddd07a43d0aa75962a4c8c72eb3e78333
Muhameddev <132582994+Muhameddev@users.noreply.github.com>
2023-05-26 11:05:42 +0500

    nisting

---
+++ Nisting.py
@@ -0,0 +1,31 @@
+country = {
+    "Italya":"Rim",
+    "O'zbekiston":"Toshkent",
+    "AQSH":"Washington DC",
+    "Tojikiston":"Dushanbe",
+    "Rossia":"Moskva"
+}
+
+cars = {
+    "model":"BWM",
+    "color":"Yellow",
+    "price":"200000",
+}
+
+
+family = {
+    "father":"Anvarjon",
+    "mather":"Xafiza",
+    "brother":"Nurmuhmmad",
+    "sister": "Sarvinoz"
+}
+
+lugatlar = [country,cars,family]
+
+oilam = f"Dadamni ismi:\n {lugatlar[2]['father']},Onamni ismi:\n {lugatlar[2]['mather']},ukamning ismi:\n{lugatlar[2]['brother']},opamning ismi:\n {lugatlar[2]['sister']}"
+print(oilam)
+
+
+
+
+
---
+++ dictionary_2.py
@@ -0,0 +1,47 @@
+country = {
+    "Italya":"Rim",
+    "O'zbekiston":"Toshkent",
+    "AQSH":"Washington DC",
+    "Tojikiston":"Dushanbe",
+    "Rossia":"Moskva"
+}
+
+cars = {
+    "model":"BWM",
+    "color":"Yellow",
+    "price":"200000",
+}
+
+
+family = {
+    "father":"Anvarjon",
+    "mather":"Xafiza",
+    "brother":"Nurmuhmmad",
+    "sister": "Sarvinoz"
+}
+
+lugatlar = [country,cars,family]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
